ca02e42a0e947db15482efcddf8bc7b6da256ab0
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2023-11-12 22:44:46 +0200

    added task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend', makeListItems(galleryItems));
+
+function makeListItems(images) {
+    return images.map(({ preview, original, description }) => 
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+                    src="${preview}"
+                    alt="${description}" 
+                    />
+            </a>
+        </li>`
+    ).join('');
+};
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
