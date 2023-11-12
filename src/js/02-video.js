fa565086e31aeb8ab0b2f6061cf9cab35c789695
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2023-11-12 23:08:02 +0200

    added task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', Throttle(onPlay, 1000));
+
+function onPlay(e) {
+    localStorage.setItem(LOCAL_KEY, e.seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+    const localTime = localStorage.getItem(LOCAL_KEY);
+    if (localTime) {
+        player.setCurrentTime(localTime);
+    }
+}
